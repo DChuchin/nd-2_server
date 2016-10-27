c58252285a0372847444930ce6737b1bffce806c
DmitryChuchin <dime10cents@gmail.com>
2016-10-27 18:18:26 +0600

    commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const https = require('https');
-const opt = {
-    port
-}
+const querystring = require('querystring');
 
-const req = https.request(url, handler);
+const data = querystring.stringify('jljfffffffffffffffffffffffffffffffffff');
+const opt = {
+    hostname: '127.0.0.1',
+    port: 3000,
+    method: 'POST',
+    path: '/add',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+    }
+};
+
+let req = https.request(opt, handler);
+req.write(data);
 req.end();
 
+req.on('error', err => console.log(err));
 req.on('response', handler);
 
 function handler(response) {
